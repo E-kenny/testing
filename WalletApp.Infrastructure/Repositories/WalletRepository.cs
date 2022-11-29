39bc765aaa02a13caf3d7ae0e534097e530d1a07
Ekene Obiasogu <ekennyobiasogu@gmail.com>
2022-11-29 10:38:46 +0100

    wallet-ft-project

---
+++ WalletApp.Infrastructure/Repositories/WalletRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WalletApp.Infrastructure.Repositories
+{
+    public class WalletRepository
+    {
+        public WalletRepository(AppDbContext context)
+        {
+
+        }
+    }
+}
